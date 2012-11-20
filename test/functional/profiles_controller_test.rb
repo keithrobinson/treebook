86db7d0f492feb640016873c485c52873bc8ee5a
Keith Robinson <keith@dkeithrobinson.com>
2012-11-20 12:22:10 -0800

    added profiles

--- test/functional/profiles_controller_test.rb
+++ test/functional/profiles_controller_test.rb
@@ -2,8 +2,12 @@ require 'test_helper'
 
 class ProfilesControllerTest < ActionController::TestCase
   test "should get show" do
-    get :show
+    get :show, id: users(:jason).profile_name
     assert_response :success
+    assert_template 'profile/show'
   end
-
+  test "should render a 404 page" do
+	get :show, id: "doesn't exist"
+	assert_response :not_found
+end
 end
