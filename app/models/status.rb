329742cf62c7b3dcef675ebbc117254e0879ee1e
Keith Robinson <keith@dkeithrobinson.com>
2012-11-20 11:39:46 -0800

    Added profile name tests.

--- app/models/status.rb
+++ app/models/status.rb
@@ -2,7 +2,7 @@ class Status < ActiveRecord::Base
   attr_accessible :content, :user_id
   belongs_to :user
 
-  validates :content, presence: true
+  validates :content, presence: true,
                       length: { minimum: 2 }
 
   validates :user_id, presence: true
